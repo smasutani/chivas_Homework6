c019cc8d20c5bb6a1d4d2ff5f9e14214b341cc6a
Sherry Masutani <sherrymasutani@mail.weber.edu>
2017-02-28 17:05:24 +0000

    adding assignment

---
+++ chivas_homework6.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  chivas_homework6.c
+ *       	Usage:  ./chivas_homework6.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2017 05:04:43 PM
+ *       Compiler:  gcc -Wall -Werr
+ *         Author:  Sherry Masutani (), sherrymasutani@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main()
+{
+
+	return 0;
+}
+
